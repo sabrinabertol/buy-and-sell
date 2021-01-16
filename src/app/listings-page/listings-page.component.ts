c757b852f70e37e7c88e0195a7606d22542a61d5
Sabrina Bertol <bertol.sabrina@gmail.com>
2021-01-16 18:39:10 +0000

    index working

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+
 
 @Component({
   selector: 'app-listings-page',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingsPageComponent implements OnInit {
 
+  listings: Listing[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeListings
   }
 
 }
